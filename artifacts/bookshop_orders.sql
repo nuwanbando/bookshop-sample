ac1b5febaa170dd297df73704a8f238d58937e31
nuwanbando <bandara.nuwan@gmail.com>
2016-03-21 23:19:39 -0400

    database

--- artifacts/bookshop_orders.sql
+++ artifacts/bookshop_orders.sql
@@ -19,10 +19,6 @@
 -- Table structure for table `orders`
 --
 
-CREATE Database `bookshop`;
-
-USE Database bookshop;
-
 DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
